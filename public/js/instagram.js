afdc6f93972bfcecd10564de23f6f3c6582c38ee
Pedro Martin <pataruco@gmail.com>
2015-06-22 16:51:54 +0100

    Form value store and return the coordinates of the input

--- public/js/instagram.js
+++ public/js/instagram.js
@@ -1,105 +1,78 @@
 
-// var socket = io('http://4f3858c8.ngrok.io');
-// var photos = [];
-
-// socket.on('connect', function () {
-//   console.log('Connected!');
-// });
-
-// socket.on('instagram', function (object) {
-//   console.log(object);
-//   $.ajax({
-//     url: 'https://api.instagram.com/v1/tags/' +  object[0].object_id + '?client_id=861d1fc4976b4559a8c2c61e01add86a',
-//     dataType: 'jsonp'
-//   }).done(function (response) {
-//     console.log(response);
-//     console.log(response.data[1]);
-//     if (photos.indexOf(response.data[0].id) === -1 && response.data[0].type === "video") {
-//       $('#photo-container').prepend('<li class="animated bounceInLeft"><video src="' + response.data[0].videos.low_resolution.url + '" controls></video></li>');
-//       photos.push(response.data[0].id);
-//     } else {
-//       console.log('duplicate');
-//     }
-//   });
-// });
+// Variables
+var searchTagForm = $('#tag_search_form');
+var searchTag = $('#search_tag'); // <--Search input folder
+var searchTagValue = ''; // <--- Value of the input form
+var loc = ''; // <--- Coordinates of the search, return i.e. {A: 48.856614, F: 2.3522219000000177} 
 
 
 // Google maps 
 
-var search = '';
-var searchTags = $('#search_tags');
+function initialize() {
+  var styles = [
+  {
+    featureType: "all",
+    stylers: [
+    { saturation: -80 
+    }]},
+    {
+      featureType: "road.arterial",
+      elementType: "geometry",
+      stylers: [
+      { hue: "#00ffee" },
+      { saturation: 50 }
+      ]},
+      {
+        featureType: "poi.business",
+        elementType: "labels",
+        stylers: [{
+         visibility: "off" 
+        }]}
+];
+  var styledMap = new google.maps.StyledMapType(styles,{
+    name: "Styled Map"
+  });
+  var mapCanvas = document.getElementById('map-canvas');
+  var mapOptions = {
+    center: new google.maps.LatLng(51.534488, -0.189897),
+    zoom: 15,
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    }
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  map.mapTypes.set('map_style', styledMap);
+  map.setMapTypeId('map_style');
+}
 
-console.log('map')
-
-// function initialize() {
-//   var styles = [
-//   {
-//     featureType: "all",
-//     stylers: [
-//     { saturation: -80 
-//     }]},
-//     {
-//       featureType: "road.arterial",
-//       elementType: "geometry",
-//       stylers: [
-//       { hue: "#00ffee" },
-//       { saturation: 50 }
-//       ]},
-//       {
-//         featureType: "poi.business",
-//         elementType: "labels",
-//         stylers: [{
-//          visibility: "off" 
-//         }]}
-// ];
-//   var styledMap = new google.maps.StyledMapType(styles,{
-//     name: "Styled Map"
-//   });
-//   var mapCanvas = document.getElementById('map-canvas');
-//   var mapOptions = {
-//     center: new google.maps.LatLng(51.534488, -0.189897),
-//     zoom: 15,
-//     mapTypeControlOptions: {
-//       mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
-//     }
-//   };
-//   var map = new google.maps.Map(mapCanvas, mapOptions);
-//   map.mapTypes.set('map_style', styledMap);
-//   map.setMapTypeId('map_style');
-// }
-
-// google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
 //Geocoder
 
 var geocoder = new google.maps.Geocoder();
-function search() {
-  console.log('search');
-    // geocoder.geocode(
-    //     {'address': searchTags.value}, 
-    //     function(results, status) { 
-    //         if (status == google.maps.GeocoderStatus.OK) { 
-    //             var loc = results[0].geometry.location;
-    //             // use loc.lat(), loc.lng()
-    //             console.log(loc);
 
-    //         } 
-    //         else {
-    //             alert("Not found: " + status); 
-    //         } 
-    //     }
-    //     debugger
-    // );
+// This function return the input value as coordinates and store it on a variables called loc
+function searchFunction (event) { 
+  event.preventDefault();
+  searchTagValue = $('#search_tag').val();
+    geocoder.geocode(
+        {'address': searchTagValue},
+        function(results, status) { 
+            if (status == google.maps.GeocoderStatus.OK) { 
+                loc = results[0].geometry.location;
+                console.log(loc);
+            } 
+            else {
+                alert("Not found: " + status); // <-- On styling change to a div to show or hide error
+            } 
+        }
+    );
 };
 
 //Event listeners
 $(document).ready(function(){ 
-  $('#search_tags').on('click', search);
+  searchTagForm.on('submit', searchFunction); // Event listener for the form,
 })
-
-
-
-
 
 
