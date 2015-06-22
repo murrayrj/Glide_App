99faceff6dbbf3fa519e1f5946a384596192abd1
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-06-22 17:02:44 +0100

    Added sockets and video limit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var instagram = require('instagram-node-lib');
@@ -17,10 +18,10 @@ app.set('view engine', 'ejs');
 instagram.set('client_id', process.env.INSTAGRAM_CLIENT_ID);
 instagram.set('client_secret', process.env.INSTAGRAM_CLIENT_SECRET);
 
-instagram.set('callback_url', 'http://4f3858c8.ngrok.io/callback');
+instagram.set('callback_url', 'http://63211216.ngrok.io/callback');
 instagram.set('maxSockets', 50);
 
-var tags = ['nyc'];
+var tags = ['videooftheday'];
 
 for (i = 0; i < tags.length; i++) {
   instagram.subscriptions.subscribe({
@@ -41,6 +42,8 @@ app.post('/callback', function(req, res) {
   console.log(req.body);
 
   var notification = req.body;
+
+  io.sockets.emit('instagram', notification);
 });
 
 server.listen(port, function() {
