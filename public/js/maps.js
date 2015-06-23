163885a14009f7d17d31ed38375212ece9915ffc
Pedro Martin <pataruco@gmail.com>
2015-06-23 10:58:35 +0100

    pin on a map

--- public/js/maps.js
+++ public/js/maps.js
@@ -2,7 +2,7 @@
 var searchTagForm = $('#tag_search_form');
 var searchTag = $('#search_tag'); // <--Search input folder
 var searchTagValue = ''; // <--- Value of the input form
-var loc = ''; // <--- Coordinates of the search, return i.e. {A: 48.856614, F: 2.3522219000000177}
+var loc = {A: 51.534488, F: -0.189897}; // <--- Coordinates of the search, return i.e. {A: 48.856614, F: 2.3522219000000177}
 var lat = 51.534488;
 var log = -0.189897;
 var map = '';
@@ -37,28 +37,25 @@ function initialize() {
   var mapCanvas = document.getElementById('map-canvas');
   var mapOptions = {
     center: new google.maps.LatLng(lat, log),
-    zoom: 15,
+    zoom: 12,
     mapTypeControlOptions: {
       mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
     }
   };
+  myLatlng = new google.maps.LatLng(lat, log);
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    title:"Hello World!"
+  });
   map = new google.maps.Map(mapCanvas, mapOptions);
   map.mapTypes.set('map_style', styledMap);
   map.setMapTypeId('map_style');
+  marker.setMap(map);
 }
 
 // Google Maps render a map on the page with the style describe previously
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-//Refresing the map canvas
-//This function it's not working
-function refreshMap() {
-  debugger;
-  console.log('renderMap');
-  google.maps.event.trigger(map, 'resize');
-}
-
 
 // This function return the input value as coordinates and store it on a variables called loc
 function searchFunction (event) { 
@@ -78,17 +75,13 @@ function searchFunction (event) {
             } 
             else {
                 alert("Not found: " + status); // <-- On styling change to a div to show or hide error
-            } 
+            }
             map.setCenter(loc);
         }
     );
-
 };
-
-
 
 //Event listeners
 $(document).ready(function(){ 
   searchTagForm.on('submit', searchFunction); // Event listener for the form,
-  // searchTagForm.on('submit', refreshMap); // Event listener for the form,
 })
